0a547caf435ab62696c301cd2b554c5f82dd35f9
Dogan Teke <dogan.teke@denkwerk.com>
2025-12-11 13:27:52 +0100

    refactor: simplify container width by removing max-width constraint in layout template

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-pureBlack text-pureWhite">
-    <div class="mx-auto max-w-7xl px-4 pb-14 pt-12 lg:px-8 sm:px-6">
+    <div class="mx-auto px-4 pb-14 pt-12 lg:px-8 sm:px-6">
       <slot />
     </div>
   </div>
